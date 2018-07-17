19ebad55d4e5cd5ffbfd148c9c56af1ee5135064
Stephen Belyea <belyea.stephen@gmail.com>
2018-07-16 21:31:37 -0400

    Keyboard and accessibility updates for modal

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -1,87 +1,96 @@
 <template>
   <div class="add-movie">
     <button 
+      ref="button"
       type="button"
       class="button is-link"
       @click.prevent="toggleModal"
     >Add Movie</button>
     <div 
+      ref="modal"
+      role="dialog"
       class="modal"
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+      :aria-hidden="!showModal"
+      :tabindex="showModal ? 0 : -1"
       :class="{ 'is-active': showModal }"
     >
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
-          <h2 class="title">Add a Movie to the Catalogue</h2>
+          <h2 
+            id="modal-title"
+            class="title"
+          >Add a Movie to the Catalogue</h2>
         </header>
         <div class="modal-card-body">
+          <p 
+            class="has-text-right"
+            id="modal-description"
+          >All fields are required</p>
           <form 
             id="add-movie"
             @submit.prevent="attemptAddMovie"
           >
-            <fieldset>
-              <div class="has-text-right">
-                <legend>All fields are required</legend>
-              </div>
-              <div class="field">
-                <label for="title">Title</label>
-                <input 
-                  type="text" 
-                  id="title"
-                  class="input"
-                  aria-required="true"
-                  v-model="movie.title"
-                  :disabled="isLoading"
-                >
-              </div>
-              <div class="field">
-                <label for="cast">Cast</label>
-                <input 
-                  type="text" 
-                  id="cast"
-                  class="input"
-                  aria-required="true"
-                  aria-describedby="cast-description"
-                  v-model="movie.cast"
-                  :disabled="isLoading"
-                >
-                <span 
-                  id="cast-description"
-                  class="description"
-                >
-                  Use a comma to separate each actor's name
-                </span>
-              </div>
-              <div class="field">
-                <label for="genre">Genre(s)</label>
-                <input 
-                  type="text" 
-                  id="genre"
-                  class="input"
-                  aria-required="true"
-                  aria-describedby="genre-description"
-                  v-model="movie.genre"
-                  :disabled="isLoading"
-                >
-                <span 
-                  id="genre-description"
-                  class="description"
-                >
-                  Use a comma to separate each genre
-                </span>
-              </div>
-              <div class="field">
-                <label for="year">Year</label>
-                <input 
-                  type="text" 
-                  id="year"
-                  class="input"
-                  aria-required="true"
-                  v-model="movie.year"
-                  :disabled="isLoading"
-                >
-              </div>
-            </fieldset>
+            <div class="field">
+              <label for="title">Title</label>
+              <input 
+                type="text" 
+                id="title"
+                class="input"
+                aria-required="true"
+                v-model="movie.title"
+                :disabled="isLoading"
+              >
+            </div>
+            <div class="field">
+              <label for="cast">Cast</label>
+              <input 
+                type="text" 
+                id="cast"
+                class="input"
+                aria-required="true"
+                aria-describedby="cast-description"
+                v-model="movie.cast"
+                :disabled="isLoading"
+              >
+              <span 
+                id="cast-description"
+                class="description"
+              >
+                Use a comma to separate each actor's name
+              </span>
+            </div>
+            <div class="field">
+              <label for="genre">Genre(s)</label>
+              <input 
+                type="text" 
+                id="genre"
+                class="input"
+                aria-required="true"
+                aria-describedby="genre-description"
+                v-model="movie.genre"
+                :disabled="isLoading"
+              >
+              <span 
+                id="genre-description"
+                class="description"
+              >
+                Use a comma to separate each genre
+              </span>
+            </div>
+            <div class="field">
+              <label for="year">Year</label>
+              <input 
+                type="text" 
+                id="year"
+                class="input"
+                aria-required="true"
+                v-model="movie.year"
+                :disabled="isLoading"
+              >
+            </div>
           </form>
         </div>
         <div class="modal-card-foot">
@@ -155,9 +164,14 @@
     watch: {
       showModal: function(show) {
         if (!show) {
+          this.$refs.button.focus();
           this.movie = {
             ...emptyMovie
           };
+        } else {
+          this.$nextTick(() => {
+            this.$refs.modal.focus();
+          })
         }
       },
     },
